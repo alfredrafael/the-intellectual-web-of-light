17c03d45a65ca2c9f2c373892707a10e1034e209
alfredrafael <alfredrafael@gmail.com>
2019-06-01 06:36:24 -0400

    Adds details to the <Choices/> cards

--- src/components/Choices/Choices.js
+++ src/components/Choices/Choices.js
@@ -22,16 +22,16 @@ const Choices = props => {
         <Row>
           <Col md="4" sm="4">
             <Card>
-              <CardBody>
+              <CardBody className='cardBody'>
                 <CardTitle>What is the Intellectual Dark Web?</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
+              {/*   <CardSubtitle>Card subtitle</CardSubtitle> */}
               </CardBody>
               <img
                 width="100%"
-                src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                src="https://i.imgur.com/cDP6ZnX.jpg"
                 alt="Card image cap"
               />
-              <CardBody>
+              <CardBody className='cardBody'>
                 <CardText>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
@@ -43,16 +43,16 @@ const Choices = props => {
           </Col>
           <Col md="4" sm="4">
             <Card>
-              <CardBody>
-                <CardTitle>Who are the greatest exponents of the intellectual dark web?</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardBody className='cardBody'>
+            <CardTitle>Who are the greatest exponents of the intellectual dark web?</CardTitle>
+              {/*   <CardSubtitle>Card subtitle</CardSubtitle> */}
               </CardBody>
               <img
                 width="100%"
-                src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                src="https://secure.meetupstatic.com/photos/event/7/c/e/6/600_474871974.jpeg"
                 alt="Card image cap"
               />
-              <CardBody>
+              <CardBody className='cardBody'>
                 <CardText>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
@@ -64,16 +64,16 @@ const Choices = props => {
           </Col>
           <Col md="4" sm="4">
             <Card>
-              <CardBody>
-                <CardTitle>What are the central concepts of the intellectual Dark Web</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardBody className='cardBody'>
+            <CardTitle>What are the central concepts of the intellectual Dark Web</CardTitle>
+              {/*   <CardSubtitle>Card subtitle</CardSubtitle> */}
               </CardBody>
               <img
                 width="100%"
-                src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180"
+                src="https://i.imgur.com/iJxqhHv.jpg"
                 alt="Card image cap"
               />
-              <CardBody>
+              <CardBody className='cardBody'>
                 <CardText>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
