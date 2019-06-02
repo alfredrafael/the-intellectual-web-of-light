7d52270ae480bceb3265c56ea8be7723b1b6fea9
alfredrafael <alfredrafael@gmail.com>
2019-06-02 17:02:47 -0400

    Adds <WhatIs/>

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import MyNavbar from './components/MyNavbar/MyNavbar';
 import MyCarousel from './components/MyCarousel/MyCarousel';
 import Choices from './components/Choices';
 import Choices2 from './components/Choices2';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 import Earth from './components/Earth/Earth';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Router>
       <MyNavbar />
       <MyCarousel />
       <Choices2/>
        
         
-      </div>
-      
+      </Router>      
     );
   }
 }
