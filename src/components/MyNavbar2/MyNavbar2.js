3e59e2dde31d38592f426e4cf2d208252bd5219d
alfredrafael <alfredrafael@gmail.com>
2019-06-03 04:38:15 -0400

    Makes <Navbar2/> change color on scroll

--- src/components/MyNavbar2/MyNavbar2.js
+++ src/components/MyNavbar2/MyNavbar2.js
@@ -3,20 +3,66 @@ import "./MyNavbar2.css";
 import darkWebLogo from "../../assets/intellectualdarkweb_png.png";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+
+
 class MyNavbar2 extends React.Component {
-  componentDidMount() {
+    
+    componentDidMount() {
+        window.onscroll = () => {
+            const nav = document.querySelector("#nav");
+            if (this.scrollY <= 10) nav.className = "nav";
+            else nav.className = "scroll";
+          };
+    }
+    
+    burgerToggle(){
+        let linksEl = document.querySelector('.narrowLinks');
+        if (linksEl.style.display === 'block') {
+            linksEl.style.display = 'none';
+        } else {
+            linksEl.style.display = 'block';
+        }
+    }
+
+    render() {
+        return (
+            <nav id='nav'>
+				<div className="navWide">
+					<div className="wideDiv">
+						<a href="#">Link 1</a>
+						<a href="#">Link 2</a>
+						<a href="#">Link 3</a>
+					</div>
+				</div>
+				<div className="navNarrow">
+					<i className="fa fa-bars fa-2x" onClick={this.burgerToggle}></i>
+					<div className="narrowLinks">
+						<a href="#" onClick={this.burgerToggle}>Link 1</a>
+						<a href="#" onClick={this.burgerToggle}>Link 2</a>
+						<a href="#" onClick={this.burgerToggle}>Link 3</a>
+					</div>
+				</div>
+			</nav>
+
+        );
+   }
+}
+export default MyNavbar2;
+
+
+
+
+
+{/* 
+
+    componentDidMount() {
     window.onscroll = () => {
       const nav = document.querySelector("#pureNavbar");
       if (this.scrollY <= 10) nav.className = "";
       else nav.className = "scroll";
     };
   }
-
-  
-  render() {
-
-    return (
-      <div>
+///////////// on render //////////////
         <nav id="pureNavbar" class="nav2Container">
           <img src={darkWebLogo} style={{ maxWidth: "4%", marginTop: '-0.5%' }} alt="page logo" />
           <span className='navTitle'>
@@ -29,13 +75,8 @@ class MyNavbar2 extends React.Component {
           <li><Link to='/'>Hello</Link></li>
           <li><Link to='/'>Hello</Link></li>
           <li><Link to='/'>Hello</Link></li>
-        {/*   <i class="fa fa-bars icon" onClick={myFunction}></i> */}
           </ul>
 
           </span>
-        </nav>
-      </div>
-    );
-  }
-}
-export default MyNavbar2;
+        </nav> 
+    */}
